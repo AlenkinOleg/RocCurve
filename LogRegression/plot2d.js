9a5e82c909f20838806763992192b7b9490ea56e
AlenkinOleg <oleg7121994@mail.ru>
2015-09-13 20:34:54 +0300

    fin

--- LogRegression/plot2d.js
+++ LogRegression/plot2d.js
@@ -1,4 +1,29 @@
 function plot2d(div) {
+	function createLabel(text){
+		var canvas = document.createElement('canvas');
+		var size = 256;
+		canvas.width = size;
+		canvas.height = size;
+		var context = canvas.getContext('2d');
+		context.fillStyle = '#ff0000';
+		context.textAlign = 'center';
+		context.font = '24px Arial';
+		context.fillText(text, size / 2, size / 2);
+		var amap = new THREE.Texture(canvas);
+		amap.needsUpdate = true;
+		var mat = new THREE.SpriteMaterial({
+			map: amap,
+			transparent: false,
+				useScreenCoordinates: false,
+				color: "black"
+		});
+
+		var sp = new THREE.Sprite(mat);
+		sp.scale.set( 10, 10, 1 );
+		return sp;    
+	}
+	var sphereGeometry = new THREE.SphereGeometry(0.1, 20, 20);
+	var sphereMaterial = new THREE.MeshBasicMaterial({color: "black", wireframe: false});
 	var svg = div.append("svg")
 		.attr("width", w + padding)
    	 	.attr("height", h + padding);
@@ -8,6 +33,12 @@ function plot2d(div) {
 	var yScale = d3.scale.linear()
 	    .domain([minValue, maxValue])
 	    .range([h, 0]);
+	var xReturnScale = d3.scale.linear()
+	    .domain([0, w])
+	    .range([minValue, maxValue]);
+	var yReturnScale = d3.scale.linear()
+	    .domain([h, 0])
+	    .range([minValue, maxValue]);
 	var xAxis = d3.svg.axis()
 		.scale(xScale)
 		.orient("bottom")
@@ -16,6 +47,55 @@ function plot2d(div) {
 		.scale(yScale)
 		.orient("left")
 		.ticks(5);
+	var colorScale = d3.scale.linear()
+		.domain([-1, 1])
+		.interpolate(d3.interpolateHcl)
+		.range(["red", "blue"]);
+	var numOfLines = 50;
+	var strokeWidthOfLine = 1.6 * w / numOfLines;
+	var arr = [];
+	for (var i = 0; i < 2 * numOfLines; i++) {
+		arr[i] = - numOfLines + i;
+	}
+	var l = lr.decisionLine();
+	var nV = lr.normalVector();
+	var ort = {x: (xScale(nV.x2) - xScale(nV.x1)) / Math.sqrt(Math.pow(xScale(nV.x2) - xScale(nV.x1), 2) + Math.pow(yScale(nV.y2) - yScale(nV.y1), 2)) * strokeWidthOfLine, y: (yScale(nV.y2) - yScale(nV.y1)) / Math.sqrt(Math.pow(xScale(nV.x2) - xScale(nV.x1), 2) + Math.pow(yScale(nV.y2) - yScale(nV.y1), 2)) * strokeWidthOfLine};
+	var grid = svg.selectAll("line").data(arr).enter().append("line")
+		.attr("x1", function(d, i) {
+			return xScale(l.x1) + d * ort.x + padding;
+		})
+		.attr("x2", function(d) {
+		
+			return xScale(l.x2) + d * ort.x + padding;
+		})
+		.attr("y1", function(d) {
+			return yScale(l.y1) + d * ort.y;
+		})
+		.attr("y2", function(d) {
+			return yScale(l.y2) + d * ort.y;
+		})
+		.attr("stroke-width", strokeWidthOfLine)
+		.attr("stroke", function(d) {
+			return colorScale(lr.probability([xReturnScale(this.getAttribute("x1") - padding), yReturnScale(this.getAttribute("y1"))]));
+		});
+	var frequence = 50;
+	var arrRect = new Array(10000);
+	var size = w / frequence;
+	svg.selectAll("rect").data(arrRect).enter().append("rect")
+		.attr("x", function(d, i) {return (i % frequence + 0.5) * size + padding - 3})
+		.attr("y", function(d, i) {return (Math.floor(i / frequence) + 0.5) * size - 3})
+		.attr("width", size - 1)
+		.attr("height", size - 1)
+		.style("fill", "white");
+	var decLine = svg.append("line")  //decLine
+		.attr("x1", xScale(l.x1) + padding)
+		.attr("x2", xScale(l.x2) + padding)
+		.attr("y1", yScale(l.y1))
+		.attr("y2", yScale(l.y2))
+		.attr("stroke", "green")
+		.attr("stroke-width", 2);
+	svg.append("line").attr("x1", padding / 2).attr("y1", 0).attr("x2", padding / 2).attr("y2", h + padding).attr("stroke", "white").attr("stroke-width", padding);
+	svg.append("line").attr("x1", 0).attr("y1", h + padding / 2).attr("x2", w + padding).attr("y2", h + padding / 2).attr("stroke", "white").attr("stroke-width", padding);
 	svg.append("g")
 		.attr("class", "axis")
 		.attr("transform", "translate(" + padding + "," + h + ")")
@@ -24,31 +104,6 @@ function plot2d(div) {
 		.attr("class", "axis")
 		.attr("transform", "translate(" + padding + ",0)")
 		.call(yAxis);
-	var colorScale = d3.scale.linear()
-		.domain([-1, 1])
-		.interpolate(d3.interpolateHcl)
-		.range(["steelblue", "red"]);
-	var numStrokes = 50;
-	var delta = (maxValue - minValue) / numStrokes; // grid
-	for (var i = minValue; i < maxValue; i += delta) {
-		for (var j = minValue; j < maxValue; j += delta) {
-			svg.append("line") //horizontal lines
-				.attr("x1", xScale(i) + padding)
-				.attr("x2", xScale(i + delta) + padding)
-				.attr("y1", yScale(j))
-				.attr("y2", yScale(j))
-				.attr("stroke", colorScale(lr.probability([i + delta / 2, j])))
-				.attr("stroke-width", 1);
-			svg.append("line") //vertical lines
-				.attr("x1", xScale(i) + padding)
-				.attr("x2", xScale(i) + padding)
-				.attr("y1", yScale(j))
-				.attr("y2", yScale(j + delta))
-				.attr("stroke", colorScale(lr.probability([i, j + delta / 2])))
-				.attr("stroke-width", 1);
-		}
-	}
-	var grid = svg.selectAll("line");
 	svg.selectAll("circle").data(dataset).enter().append("circle")
 		.attr("cx", function(d) {return xScale(d[0]) + padding;})
    		.attr("cy", function(d) {return yScale(d[1]);})
@@ -57,27 +112,12 @@ function plot2d(div) {
    				return "blue";
    		})
    		.attr("r", 3);
-	var l = lr.decisionLine();
-	var decLine = svg.append("line")  //decLine
-		.attr("x1", xScale(l.x1) + padding)
-		.attr("x2", xScale(l.x2) + padding)
-		.attr("y1", yScale(l.y1))
-		.attr("y2", yScale(l.y2))
-		.attr("stroke", "gray")
-		.attr("stroke-width", 2);
-	var nV = lr.normalVector();
-	var xReturnScale = d3.scale.linear()
-	    .domain([0, w])
-	    .range([minValue, maxValue]);
-	var yReturnScale = d3.scale.linear()
-	    .domain([h, 0])
-	    .range([minValue, maxValue]);
 	var nvLine = svg.append("line")  //normalVector
 		.attr("x1", xScale(nV.x1) + padding)
 		.attr("x2", xScale(nV.x2) + padding)
 		.attr("y1", yScale(nV.y1))
 		.attr("y2", yScale(nV.y2))
-		.attr("stroke", "gray")
+		.attr("stroke", "green")
 		.attr("stroke-width", 2);
 		//.call(drag);
 	var svgDrag = d3.behavior.drag()
@@ -97,28 +137,62 @@ function plot2d(div) {
 			return;
 		}
 		else {
+			l = lr.decisionLine();
+			lr.coeff = [nV.x2 - nV.x1, nV.y2 - nV.y1];
+			lr.bias = returnBias(nV.x1, nV.y1, lr.coeff[0], lr.coeff[1]);
+			ort = {x: (xScale(nV.x2) - xScale(nV.x1)) / Math.sqrt(Math.pow(xScale(nV.x2) - xScale(nV.x1), 2) + Math.pow(yScale(nV.y2) - yScale(nV.y1), 2)) * strokeWidthOfLine, y: (yScale(nV.y2) - yScale(nV.y1)) / Math.sqrt(Math.pow(xScale(nV.x2) - xScale(nV.x1), 2) + Math.pow(yScale(nV.y2) - yScale(nV.y1), 2)) * strokeWidthOfLine};
+			grid
+				.attr("x1", function(d) {
+					return xScale(l.x1) + d * ort.x + padding;
+				})
+				.attr("x2", function(d) {
+					return xScale(l.x2) + d * ort.x + padding;
+				})
+				.attr("y1", function(d) {
+					return yScale(l.y1) + d * ort.y;
+				})
+				.attr("y2", function(d) {
+					return yScale(l.y2) + d * ort.y;
+				})
+				.attr("stroke", function(d) {
+					return colorScale(lr.probability([xReturnScale(xScale(nV.x1) + d * ort.x), yReturnScale(yScale(nV.y1) + d * ort.y)]))
+				});
 			nvLine
 				.attr("x2", xScale(nV.x2) + padding)
 				.attr("y2", yScale(nV.y2))
 				.attr("x1", xScale(nV.x1) + padding)
 				.attr("y1", yScale(nV.y1));
-			lr.coeff = [nV.x2 - nV.x1, nV.y2 - nV.y1];
-			lr.bias = returnBias(nV.x1, nV.y1, lr.coeff[0], lr.coeff[1]);
-			grid
-				.attr("stroke", function() {
-					return colorScale(lr.probability([xReturnScale(this.getAttribute("x1") - padding), yReturnScale(this.getAttribute("y1"))]));
-				});
-			l = lr.decisionLine();
 			decLine
 				.attr("x1", xScale(l.x1) + padding)
 				.attr("x2", xScale(l.x2) + padding)
 				.attr("y1", yScale(l.y1))
 				.attr("y2", yScale(l.y2));
-			data3D.transition().ease(ease).duration(0)
-			.attr("translation", function(d, i) {
-				if (labels[i]) return xSc3D(d[0]) + " " + ySc3D(lr.loglossPenalties(d)[0]) + " " + zSc3D(d[1]);
-				return xSc3D(d[0]) + " " + ySc3D(lr.loglossPenalties(d)[1]) + " " + zSc3D(d[1]);
-			});
+			scaleZReturn = d3.scale.linear()
+				.domain([0, 20])
+				.range([0, maxTickZ()]);
+			scaleZ = d3.scale.linear()
+				.domain([0, maxTickZ()])
+				.range([0, 20]);
+			ticksValZ = scaleZ.ticks(numTicksZ);
+			for (var i = 0; i < ticksZ.length; i++){
+				scene.remove(ticksZ[i]);
+				scene.remove(tickLabelZ[i]);
+			}
+			for (var i = 0; i < ticksValZ.length; i++){
+				ticksZ[i] = new THREE.Mesh(sphereGeometry, sphereMaterial);
+				ticksZ[i].position.set(0, scaleZ(ticksValZ[i]), 0);
+				scene.add(ticksZ[i]);
+				tickLabelZ[i] = createLabel(ticksValZ[i]);
+				tickLabelZ[i].position.set(-0.25, scaleZ(ticksValZ[i]), -0.25);
+				scene.add(tickLabelZ[i]);
+			};
+			for (var i = 0; i < nSamples; i++){
+				if (labels[i]) spheres[i].translateY(- spheres[i].position.y + scaleZ(lr.loglossPenalties(dataset[i])[0]))
+				else spheres[i].translateY(- spheres[i].position.y + scaleZ(lr.loglossPenalties(dataset[i])[1]));
+			};
+			surfaceBlue.geometry = new THREE.ParametricGeometry(surfaceFunctionBlue, 16, 16);
+			surfaceRed.geometry = new THREE.ParametricGeometry(surfaceFunctionRed, 16, 16);
+			renderer.render(scene, camera);
 		}
 	}
 	svg.call(svgDrag);
--- LogRegression/plot3d.js
+++ LogRegression/plot3d.js
@@ -1,219 +1,221 @@
- var data3d;
- var xSc3D, ySc3D, zSc3D;
- var ease;
- //Some variables are defined at main.html (like labels, dataset, ...)
-function scatterPlot3d(parent)
-{
-	  var x3d = parent  
-		.append("x3d")
-		  .style( "width", parseInt(parent.style("width"))+"px" )
-		  .style( "height", parseInt(parent.style("height"))+"px" )
-		  .style( "border", "none" )
-	  x3d.append("param")
-		.attr("name", "disableRotation")
-		.attr("value", "true")
-		  
-	  var scene = x3d.append("scene")
-	  scene.append("orthoviewpoint")
-		 .attr( "centerOfRotation", [5, 5, 5])
-		 .attr( "fieldOfView", [-5, -5, 15, 15])
-		 .attr( "orientation", [-0.5, 1, 0.2, 1.12*Math.PI/4])
-		 .attr( "position", [8, 4, 15])
-	  parent.onmousemove = function() {console.log("hi")}
-	  var axisRange = [0, 8];
-	  var scales = [];
-	  var initialDuration = 0;
-	  var defaultDuration = 800;
-	  ease = 'linear';
-	  var time = 0;
-	  var axisKeys = ["x", "y", "z"];
-  // Helper functions for initializeAxis() and drawAxis()
-  function axisName( name, axisIndex ) {
-    return ['x','y','z'][axisIndex] + name;
-  }
+function scatterPlot3d(div, width, height) {
+			scaleReturn = d3.scale.linear()
+				.domain(axisRange)
+				.range([-4, 4]);
+			scale = d3.scale.linear()
+				.domain([-4, 4])
+				.range(axisRange);
 
-  function constVecWithAxisValue( otherValue, axisValue, axisIndex ) {
-    var result = [otherValue, otherValue, otherValue];
-    result[axisIndex] = axisValue;
-    return result;
-  }
+			init();
+			render();
+			
+			function createLabel(text){
+				var canvas = document.createElement('canvas');
+				var size = 256;
+				canvas.width = size;
+				canvas.height = size;
+				var context = canvas.getContext('2d');
+				context.fillStyle = '#ff0000';
+				context.textAlign = 'center';
+				context.font = '24px Arial';
+				context.fillText(text, size / 2, size / 2);
 
-  // Used to make 2d elements visible
-  function makeSolid(selection, color) {
-    selection.append("appearance")
-      .append("material")
-         .attr("diffuseColor", color||"black")
-    return selection;
-  }
+				var amap = new THREE.Texture(canvas);
+				amap.needsUpdate = true;
 
-  // Initialize the axes lines and labels.
-  function initializePlot() {
-    initializeAxis(0, [-4, 4]);
-    initializeAxis(1, [0, Math.max(lr.loglossPenalties([-4, -4])[0], lr.loglossPenalties([4, 4])[0], lr.loglossPenalties([-4, -4])[1], lr.loglossPenalties([4, 4])[1])]);
-    initializeAxis(2, [-4, 4]);
-  }
+				var mat = new THREE.SpriteMaterial({
+					map: amap,
+					transparent: false,
+					useScreenCoordinates: false,
+					color: "black"
+				});
 
-  function initializeAxis( axisIndex, axisDomain )
-  {
-    var key = axisKeys[axisIndex];
-    drawAxis( axisIndex, key, initialDuration, axisDomain );
+				var sp = new THREE.Sprite(mat);
+				sp.scale.set( 10, 10, 1 );
+				return sp;    
+			}
+			
+			function initializeAxes(axisRange, numTicks) {
+				var ticksVal = scale.ticks(numTicks);
+				var ticksX = new Array();
+				var tickMaterial = new THREE.LineBasicMaterial({
+					color: "black"
+				});
+				var sphereGeometry = new THREE.SphereGeometry(0.1, 20, 20);
+				var sphereMaterial = new THREE.MeshBasicMaterial({color: "black", wireframe: false});
+				for (var i = 0; i < ticksVal.length; i++) {
+					ticksX[i] = new THREE.Mesh(sphereGeometry,sphereMaterial);
+					ticksX[i].position.set(scale(ticksVal[i]), 0, 0);
+					scene.add(ticksX[i]);
+					var tickLabel = createLabel(ticksVal[i]);
+					tickLabel.position.set(scale(ticksVal[i]), 0.5, -0.5);
+					scene.add(tickLabel);
+				};
+				var ticksY = new Array();
+				for (var i = 0; i < ticksVal.length; i++) {
+					ticksY[i] = new THREE.Mesh(sphereGeometry,sphereMaterial);
+					ticksY[i].position.set(0, 0, scale(ticksVal[i]));
+					scene.add(ticksY[i]);
+					var tickLabel = createLabel(ticksVal[i]);
+					tickLabel.position.set(-0.5, 0.5, scale(ticksVal[i]));
+					scene.add(tickLabel);
+				};
+				
+				var axisMaterial = new THREE.LineBasicMaterial({
+					color: "black"
+				});
+				var xGeometry = new THREE.Geometry();
+				xGeometry.vertices.push(
+					new THREE.Vector3(20, 0, 0),
+					new THREE.Vector3(0, 0, 0)
+				);
+				var zGeometry = new THREE.Geometry();
+				zGeometry.vertices.push(
+					new THREE.Vector3(0, 20, 0),
+					new THREE.Vector3(0, 0, 0)
+				);
+				var yGeometry = new THREE.Geometry();
+				yGeometry.vertices.push(
+					new THREE.Vector3(0, 0, 20),
+					new THREE.Vector3(0, 0, 0)
+				);
+				var xAxis = new THREE.Line(xGeometry, axisMaterial);
+				var yAxis = new THREE.Line(yGeometry, axisMaterial);
+				var zAxis = new THREE.Line(zGeometry, axisMaterial);
+				scene.add(xAxis);
+				scene.add(zAxis);
+				scene.add(yAxis);
+				//add Grid
+				for (var i = 1; i < ticksVal.length; i++){
+					var lineMaterial = new THREE.LineBasicMaterial({color: "gray"});
+					var lineGeometryX = new THREE.Geometry();
+					lineGeometryX.vertices.push(
+						new THREE.Vector3(scale(ticksVal[i]), 0, scale(-4)),
+						new THREE.Vector3(scale(ticksVal[i]), 0, scale(4))
+					);
+					var lineX = new THREE.Line(lineGeometryX, lineMaterial);
+					scene.add(lineX);
+					var lineGeometryY = new THREE.Geometry();
+					lineGeometryY.vertices.push(
+						new THREE.Vector3(scale(4), 0, scale(ticksVal[i])),
+						new THREE.Vector3(scale(-4), 0, scale(ticksVal[i]))
+					);
+					var lineY = new THREE.Line(lineGeometryY, lineMaterial);
+					scene.add(lineY);
+				}
+				ticksValZ = scaleZ.ticks(numTicksZ);
+				for (var i = 0; i < ticksValZ.length; i++) {
+					ticksZ[i] = new THREE.Mesh(sphereGeometry,sphereMaterial);
+					ticksZ[i].position.set(0, scaleZ(ticksValZ[i]), 0);
+					scene.add(ticksZ[i]);
+					tickLabelZ[i] = createLabel(ticksValZ[i]);
+					tickLabelZ[i].position.set(-0.25, scaleZ(ticksValZ[i]), -0.25);
+					scene.add(tickLabelZ[i]);
+				};
+			}
 
-    var scaleMin = axisRange[0];
-    var scaleMax = axisRange[1];
+			function animate() {
+				requestAnimationFrame(animate);
+				controls.update();
+			}
 
-    // the axis line
-    var newAxisLine = scene.append("transform")
-         .attr("class", axisName("Axis", axisIndex))
-         .attr("rotation", ([[0,0,0,0],[0,0,1,Math.PI/2],[0,1,0,-Math.PI/2]][axisIndex]))
-      .append("shape")
-    newAxisLine
-      .append("appearance")
-      .append("material")
-        .attr("emissiveColor", "lightgray")
-    newAxisLine
-      .append("polyline2d")
-         // Line drawn along y axis does not render in Firefox, so draw one
-         // along the x axis instead and rotate it (above).
-        .attr("lineSegments", "0 0," + scaleMax + " 0")
+			function init() {
 
-   // axis labels
-   var newAxisLabel = scene.append("transform")
-       .attr("class", axisName("AxisLabel", axisIndex))
-       .attr("translation", constVecWithAxisValue( 0, scaleMin + 1.1 * (scaleMax-scaleMin), axisIndex ))
+				camera = new THREE.PerspectiveCamera( 60, width / height, 1, 1000 );
+				camera.position.z = 30;
+				camera.position.y = 30;
+				camera.position.x = 30;
 
-   var newAxisLabelShape = newAxisLabel
-     .append("billboard")
-       .attr("axisOfRotation", "0 0 0") // face viewer
-     .append("shape")
-     .call(makeSolid)
+				scene = new THREE.Scene();
+				scene.fog = new THREE.FogExp2( "white", 0.002 );
 
-   var labelFontSize = 0.6;
+				// world
+				var geometry = new THREE.PlaneGeometry( 5, 20, 32 );
+				var axes = new THREE.AxisHelper( 20 );
+				scene.add(axes);
 
-   newAxisLabelShape
-     .append("text")
-       .attr("class", axisName("AxisLabelText", axisIndex))
-       .attr("solid", "true")
-       .attr("string", key)
-    .append("fontstyle")
-       .attr("size", labelFontSize)
-       .attr("family", "SANS")
-       .attr("justify", "END MIDDLE" )
-  }
+				var surfaceGeometryBlue = new THREE.ParametricGeometry(surfaceFunctionBlue, 16, 16);
 
-  // Assign key to axis, creating or updating its ticks, grid lines, and labels.
-  function drawAxis( axisIndex, key, duration, axisDomain ) {
+				var materialBlue = new THREE.MeshBasicMaterial({
+					color: "blue",
+					specular: 0x080808,
+					side: THREE.DoubleSide,
+					transparent: true,
+					opacity: 0.2,
+					depthTest: false
+				});
+				
+				surfaceBlue = new THREE.Mesh( surfaceGeometryBlue, materialBlue );
+				scene.add(surfaceBlue);
+				var surfaceGeometryRed = new THREE.ParametricGeometry(surfaceFunctionRed, 16, 16);
 
-    var scale = d3.scale.linear()
-      .domain( axisDomain ) // demo data range
-      .range( axisRange )
-    
-    scales[axisIndex] = scale;
+				var materialRed = new THREE.MeshBasicMaterial({
+					color: "red",
+					specular: 0x080808,
+					side: THREE.DoubleSide,
+					transparent: true,
+					opacity: 0.2,
+					depthTest: false
+				});
+				
+				surfaceRed = new THREE.Mesh( surfaceGeometryRed, materialRed );
+				scene.add(surfaceRed);
+				var numTicks = 8;
+				initializeAxes(axisRange, numTicks);
+				
+				var sphereGeometry = new THREE.SphereGeometry(0.3, 20, 20);
+				var sphereMaterialRed = new THREE.MeshBasicMaterial({color: "red", wireframe: false});
+				var sphereMaterialBlue = new THREE.MeshBasicMaterial({color: "blue", wireframe: false});
+				for (var i = 0; i < nSamples; i++){
+					if (labels[i]){
+						spheres[i] = new THREE.Mesh(sphereGeometry, sphereMaterialRed);
+						spheres[i].position.y = scaleZ(lr.loglossPenalties(dataset[i])[0]);
+					}
+					else {
+						spheres[i] = new THREE.Mesh(sphereGeometry, sphereMaterialBlue);
+						spheres[i].position.y = scaleZ(lr.loglossPenalties(dataset[i])[1]);
+					}
+					spheres[i].position.x = scale(dataset[i][0]);
+					spheres[i].position.z = scale(dataset[i][1]);
+					scene.add(spheres[i]);
+				}
+				
+				// lights
 
-    var numTicks = 8;
-    var tickSize = 0.1;
-    var tickFontSize = 0.5;
+				light = new THREE.DirectionalLight( 0xffffff );
+				light.position.set( 1, 1, 1 );
+				scene.add( light );
 
-    // ticks along each axis
-    var ticks = scene.selectAll( "."+axisName("Tick", axisIndex) )
-       .data( scale.ticks( numTicks ));
-    var newTicks = ticks.enter()
-      .append("transform")
-        .attr("class", axisName("Tick", axisIndex));
-    newTicks.append("shape").call(makeSolid)
-      .append("box")
-        .attr("size", tickSize + " " + tickSize + " " + tickSize);
-    // enter + update
-    ticks.transition().duration(duration)
-      .attr("translation", function(tick) { 
-         return constVecWithAxisValue( 0, scale(tick), axisIndex ); })
-    ticks.exit().remove();
+				light = new THREE.DirectionalLight( 0x002288 );
+				light.position.set( -1, -1, -1 );
+				scene.add( light );
 
-    // tick labels
-    var tickLabels = ticks.selectAll("billboard shape text")
-      .data(function(d) { return [d]; });
-    var newTickLabels = tickLabels.enter()
-      .append("billboard")
-         .attr("axisOfRotation", "0 0 0")     
-      .append("shape")
-      .call(makeSolid)
-    newTickLabels.append("text")
-      .attr("string", scale.tickFormat(10))
-      .attr("solid", "true")
-      .append("fontstyle")
-        .attr("size", tickFontSize)
-        .attr("family", "SANS")
-        .attr("justify", "END MIDDLE" );
-    tickLabels // enter + update
-      .attr("string", scale.tickFormat(10))
-    tickLabels.exit().remove();
+				light = new THREE.AmbientLight( 0x222222 );
+				scene.add( light );
+				
+				// renderer
 
-    // base grid lines
-    if (axisIndex==0 || axisIndex==2) {
+				renderer = new THREE.WebGLRenderer( { antialias: false } );
+				renderer.setClearColor( scene.fog.color );
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( width, height );
+				
+				controls = new THREE.OrbitControls(camera, renderer.domElement);
+				controls.damping = 0.4;
+				controls.addEventListener( 'change', render );
 
-      var gridLines = scene.selectAll( "."+axisName("GridLine", axisIndex))
-         .data(scale.ticks( numTicks ));
-      gridLines.exit().remove();
-      
-      var newGridLines = gridLines.enter()
-        .append("transform")
-          .attr("class", axisName("GridLine", axisIndex))
-          .attr("rotation", axisIndex==0 ? [0,1,0, -Math.PI/2] : [0,0,0,0])
-        .append("shape")
+				div.appendChild(renderer.domElement);
+				window.addEventListener('resize', onWindowResize, false);
+				animate();
+			}
 
-      newGridLines.append("appearance")
-        .append("material")
-          .attr("emissiveColor", "gray")
-      newGridLines.append("polyline2d");
-
-      gridLines.selectAll("shape polyline2d").transition().duration(duration)
-        .attr("lineSegments", "0 0, " + axisRange[1] + " 0")
-
-      gridLines.transition().duration(duration)
-         .attr("translation", axisIndex==0
-            ? function(d) { return scale(d) + " 0 0"; }
-            : function(d) { return "0 0 " + scale(d); }
-          )
-    }  
-  }
-
-  function plotData() {
-	var x = scales[0], y = scales[1], z = scales[2];
-    var sphereRadius = 0.1;
-
-    // Draw a sphere at each x,y,z coordinate.
-    var datapoints = scene.selectAll(".datapoint").data(dataset);
-    datapoints.exit().remove()
-
-    var newDatapoints = datapoints.enter()
-      .append("transform")
-        .attr("class", "datapoint")
-        .attr("scale", [sphereRadius, sphereRadius, sphereRadius])
-      .append("shape");
-    newDatapoints
-      .append("appearance")
-      .append("material");
-    newDatapoints
-      .append("sphere")
-       // Does not work on Chrome; use transform instead
-       //.attr("radius", sphereRadius)
-
-    datapoints.select("shape appearance material")
-        .attr("diffuseColor", function(d, i){
-			if (labels[i]) return 'red';
-			return 'blue';
-		} )
-
-    datapoints.transition().ease(ease).duration(0)
-        .attr("translation", function(d, i) {
-			if (labels[i]) return x(d[0]) + " " + y(lr.loglossPenalties(d)[0]) + " " + z(d[1]);
-			return x(d[0]) + " " + y(lr.loglossPenalties(d)[1]) + " " + z(d[1]);
-		})
-	data3D = datapoints;
-	xSc3D = x;
-	ySc3D = y;
-	zSc3D = z;
-  }
-
-  initializePlot();
-  plotData(defaultDuration);
+			function onWindowResize() {
+				camera.aspect = width / height;
+				camera.updateProjectionMatrix();
+				renderer.setSize( width, height );
+				render();
+			}
+			function render() {
+				renderer.render( scene, camera );
+			}
 }
